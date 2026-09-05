5f3d4c76ff96b991f43cd9d3562332dc87262985
Dibyanshu Samal <dibyanshusamal787@gmail.com>
2026-09-05 20:58:51 +0530

    Delete frontend directory

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ScrollSequence from "@/components/ScrollSequence";
-
-export default function Home() {
-  return (
-    <main style={{ minHeight: "100vh", backgroundColor: "#000" }}>
-      <ScrollSequence />
-    </main>
-  );
-}
